7e01a80ee26008489cf7b320bef2b02552e2cbea
scottbw <scott.bradley.wilson@gmail.com>
2012-05-16 15:14:20 +0100

    Added composite Ratings model

---
+++ src/main/java/uk/ac/bolton/spaws/model/IRatings.java
@@ -0,0 +1,34 @@
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.bolton.spaws.model;
+
+public interface IRatings extends IParadata{
+	
+	public static String VERB = "allrated";
+	
+	public abstract double getAverage();
+	public abstract void setAverage(double average);
+	
+	public abstract int getMin();
+	public abstract void setMin(int min);
+	
+	public abstract int getMax();
+	public abstract void setMax(int max);
+	
+	public abstract int getSample();
+	public abstract void setSample(int sample);
+
+}
---
+++ src/main/java/uk/ac/bolton/spaws/model/impl/Ratings.java
@@ -0,0 +1,73 @@
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.bolton.spaws.model.impl;
+
+import com.navnorth.learningregistry.LRActivity;
+
+import uk.ac.bolton.spaws.model.IRatings;
+
+public class Ratings implements IRatings {
+	
+	private double average = 0.0;
+	private int min = 0;
+	private int max = 5;
+	private int sample = 1;
+
+	public String getVerb() {
+		return VERB;
+	}
+
+	public void addMeasure(LRActivity activity) {
+		activity.addMeasureToVerb(VERB, getAverage(), getMin(), getMax(), getSample());
+	}
+
+	public String getContent() {
+		return null;
+	}
+
+	public double getAverage() {
+		return average;
+	}
+
+	public void setAverage(double average) {
+		this.average = average;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public int getSample() {
+		return sample;
+	}
+
+	public void setSample(int sample) {
+		this.sample = sample;
+	}
+
+}
