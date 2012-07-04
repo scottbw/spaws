1cbc96c8535c4e9258c1d339eee762a87850780a
scottbw <scott.bradley.wilson@gmail.com>
2012-07-04 15:12:51 +0100

    Added test case for aggregate Stats

--- src/test/java/uk/ac/bolton/spaws/integration/StatsTest.java
+++ src/test/java/uk/ac/bolton/spaws/integration/StatsTest.java
@@ -15,19 +15,18 @@
  */
 package uk.ac.bolton.spaws.integration;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import uk.ac.bolton.spaws.ParadataManager;
 import uk.ac.bolton.spaws.model.INode;
+import uk.ac.bolton.spaws.model.IStats;
 import uk.ac.bolton.spaws.model.ISubmission;
 import uk.ac.bolton.spaws.model.impl.Node;
-import uk.ac.bolton.spaws.model.impl.Stats;
-import uk.ac.bolton.spaws.model.impl.Submission;
 import uk.ac.bolton.spaws.model.impl.Submitter;
 
 public class StatsTest {
@@ -35,40 +34,58 @@ public class StatsTest {
 	private static final String MIMAS_TEST_NODE_URL = "http://alpha.mimas.ac.uk";
 	private static final String WIDGET_URI = "http://wookie.apache.org/widgets/butterfly";
 	private static INode node;
+	
 	@BeforeClass
 	public static void setup() throws Exception{
 		
 		node = new Node(new URL(MIMAS_TEST_NODE_URL), "fred", "flintstone");
 	
-		ParadataManager manager = new ParadataManager(new Submitter(), node);
-
-		List<ISubmission> submissions = new ArrayList<ISubmission>();
-		
-		Submitter site1 = new Submitter();
-		site1.setSubmitter("SPAWS-TEST-SITE-1");
-		Stats site1stats = new Stats();
-		site1stats.setDownloads(11);
-		site1stats.setEmbeds(1);
-		site1stats.setLikes(1);
-		site1stats.setViews(111);
-		
-		Submitter site2 = new Submitter();
-		site2.setSubmitter("SPAWS-TEST-SITE-2");
-		Stats site2stats = new Stats();
-		site2stats.setDownloads(22);
-		site2stats.setViews(222);
-		
-		
-		submissions.add(new Submission(site1, null, site1stats, WIDGET_URI));
-		submissions.add(new Submission(site2, null, site2stats, WIDGET_URI));
-		
-		manager.publishSubmissions(submissions);
-	
+//		ParadataManager manager = new ParadataManager(new Submitter(), node);
+//
+//		List<ISubmission> submissions = new ArrayList<ISubmission>();
+//		
+//		Submitter site1 = new Submitter();
+//		site1.setSubmitter("SPAWS-TEST-SITE-1");
+//		Stats site1stats = new Stats();
+//		site1stats.setDownloads(11);
+//		site1stats.setEmbeds(1);
+//		site1stats.setLikes(1);
+//		site1stats.setViews(111);
+//		
+//		Submitter site2 = new Submitter();
+//		site2.setSubmitter("SPAWS-TEST-SITE-2");
+//		Stats site2stats = new Stats();
+//		site2stats.setDownloads(22);
+//		site2stats.setViews(222);
+//		
+//		
+//		submissions.add(new Submission(site1, null, site1stats, WIDGET_URI));
+//		submissions.add(new Submission(site2, null, site2stats, WIDGET_URI));
+//		
+//		manager.publishSubmissions(submissions);
 	}
 	
 	@Test
-	public void test(){
+	public void test() throws Exception{
 		
+		ParadataManager manager = new ParadataManager(new Submitter(), node);
+		int downloads = 0;
+		int views = 0;
+		int embeds = 0;
+		int likes = 0;
+		
+		for (ISubmission submission: manager.getExternalStats(WIDGET_URI)){
+			IStats stats = (IStats)submission.getAction();
+			downloads += stats.getDownloads();
+			views += stats.getViews();
+			embeds += stats.getEmbeds();
+			likes += stats.getLikes();
+		}
+		
+		assertEquals(33, downloads);
+		assertEquals(333, views);
+		assertEquals(1, embeds);
+		assertEquals(1, likes);
 	}
 
 }
